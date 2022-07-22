888385f848c5dadfebddca6e1ace2b381e8f2acc
Vojislav Stojkovic <zinthys@gmail.com>
2022-07-21 20:14:32 -0700

    Placeholder for server browser

--- src/gui/main_menu.rs
+++ src/gui/main_menu.rs
@@ -1,5 +1,5 @@
 use fltk::app;
-use fltk::button::Button;
+use fltk::button::{Button, RadioButton};
 use fltk::dialog;
 use fltk::enums::FrameType;
 use fltk::group::Column;
@@ -7,37 +7,65 @@ use fltk::prelude::*;
 
 use super::alert_not_implemented;
 
-pub(super) struct MainMenu;
+pub(super) struct MainMenu {
+    pub(super) group: Column,
+    continue_btn: Button,
+    online_btn: RadioButton,
+}
 
 impl MainMenu {
-    pub(super) fn new(on_continue: impl Fn() -> std::io::Result<()> + 'static) -> Self {
+    pub(super) fn new() -> Self {
         let mut group = Column::default_fill();
         group.set_margin(10);
         group.set_pad(10);
 
-        let _continue_btn = make_button("Continue", move |_| {
+        let continue_btn = make_button(Button::default, "Continue");
+        let online_btn = make_button(RadioButton::default, "Online");
+        let mut singleplayer_btn = make_button(Button::default, "Singleplayer");
+        let mut coop_btn = make_button(Button::default, "Co-op");
+        let mut mods_btn = make_button(Button::default, "Mods");
+        let mut settings_btn = make_button(Button::default, "Settings");
+        let mut exit_btn = make_button(Button::default, "Exit");
+
+        group.end();
+
+        singleplayer_btn.set_callback(alert_not_implemented);
+        coop_btn.set_callback(alert_not_implemented);
+        mods_btn.set_callback(alert_not_implemented);
+        settings_btn.set_callback(alert_not_implemented);
+        exit_btn.set_callback(|_| app::quit());
+
+        Self {
+            group,
+            continue_btn,
+            online_btn,
+        }
+    }
+
+    pub(super) fn set_on_continue(
+        &mut self,
+        on_continue: impl Fn() -> std::io::Result<()> + 'static,
+    ) {
+        self.continue_btn.set_callback(move |_| {
             if let Err(err) = on_continue() {
                 dialog::alert_default(&format!("Failed to launch Conan Exiles:\n{}", err))
             }
         });
-        let _online_btn = make_button("Online", alert_not_implemented);
-        let _singleplayer_btn = make_button("Singleplayer", alert_not_implemented);
-        let _coop_btn = make_button("Co-op", alert_not_implemented);
-        let _mods_btn = make_button("Mods", alert_not_implemented);
-        let _settings_btn = make_button("Settings", alert_not_implemented);
-        let _exit_btn = make_button("Exit", |_| app::quit());
+    }
 
-        group.end();
-
-        Self
+    pub(super) fn set_on_online(&mut self, mut on_online: impl FnMut() + 'static) {
+        self.online_btn.set_callback(move |_| on_online());
     }
 }
 
-fn make_button<F: FnMut(&mut Button) + 'static>(text: &str, callback: F) -> Button {
-    let mut button = Button::default().with_label(text);
-    button.set_frame(FrameType::RoundUpBox);
-    button.set_down_frame(FrameType::RoundDownBox);
+fn make_button<B, C>(ctor: C, text: &str) -> B
+where
+    B: ButtonExt,
+    C: FnOnce() -> B,
+{
+    let mut button = ctor().with_label(text);
+    button.set_frame(FrameType::PlasticThinUpBox);
+    button.set_down_frame(FrameType::PlasticThinDownBox);
     button.clear_visible_focus();
-    button.set_callback(callback);
     button
 }
--- src/gui/mod.rs
+++ src/gui/mod.rs
@@ -1,5 +1,10 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+
 use fltk::dialog;
+use fltk::group::{Group, Row};
 use fltk::prelude::*;
+use fltk::text::TextDisplay;
 use fltk::window::Window;
 
 mod main_menu;
@@ -12,12 +17,45 @@ pub struct LauncherWindow {
 
 impl LauncherWindow {
     pub fn new(on_continue: impl Fn() -> std::io::Result<()> + 'static) -> Self {
-        let mut window = Window::default().with_size(400, 300);
+        let mut window = Window::default().with_size(1280, 760);
         window.set_label("BUGLE");
 
-        let _main_menu = MainMenu::new(on_continue);
+        let mut main_group = Row::default_fill();
+
+        let mut _main_menu = MainMenu::new();
+
+        let content_group = Group::default_fill();
+
+        let welcome_group = Group::default_fill();
+        let _welcome_text = TextDisplay::default()
+            .with_label("Welcome to BUGLE: Butt-Ugly Game Launcher For Exiles")
+            .center_of_parent();
+        welcome_group.end();
+
+        let mut online_group = Group::default_fill();
+        let _placeholder = TextDisplay::default()
+            .with_label("Server Browser Placeholder")
+            .center_of_parent();
+        online_group.end();
+        online_group.hide();
+
+        content_group.end();
+
+        main_group.set_size(&_main_menu.group.as_group().unwrap(), 300);
+        main_group.end();
 
         window.end();
+
+        let active_content_group = Rc::new(RefCell::new(welcome_group));
+
+        _main_menu.set_on_continue(on_continue);
+        {
+            let active_content_group = active_content_group.clone();
+            _main_menu.set_on_online(move || {
+                active_content_group.borrow_mut().hide();
+                online_group.show();
+            });
+        }
 
         Self { window }
     }
